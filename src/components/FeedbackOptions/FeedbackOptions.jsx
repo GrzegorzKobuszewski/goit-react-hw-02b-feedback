3ea0eecdb23844adaa6141f4413a90a6a4210183
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2023-09-18 09:24:09 +0200

    Zadanie + Sylowanie

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import styles from './FeedbackOptions.module.css';
+
+
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+    return (
+        <ul className={styles.list}>
+            {options.map(option => (
+                <li key={option} className={styles.item}>
+                    <button
+                        onClick={() => onLeaveFeedback(option)}
+                        className={styles.button}
+                    >
+                        {option}
+                    </button>
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+FeedbackOptions.propTypes = {
+    options: PropTypes.array.isRequired,
+    onLeaveFeedback: PropTypes.func.isRequired,
+};
